520174f62235945a45299f6a03218fc2511c7871
Manan Mehra <mananmehra330@gmail.com>
2024-04-29 00:56:49 +0530

    bigM code added

---
+++ bigM.m
@@ -0,0 +1,84 @@
+format short
+clear all
+clc
+
+%input parameters
+variables = ["x1","x2","s2","s3","a1","a2","sol"];
+M=1000;
+C=[-2 -1 0 0 -M -M 0];
+A=[3 1 0 0 1 0 3 ; 4 3 -1 0 0 1 6 ; 1 2 0 1 0 0 3];
+s=eye(size(A,1));
+
+BV=[];
+for j=1:size(s,2)
+    for i=1:size(A,2)
+        if A(:,i)==s(:,j)
+            BV = [BV i];
+        end
+    end
+end
+
+zjcj = C(BV)*A - C;
+
+%printing the table
+ZCj = [zjcj;A];
+simpTable = array2table(ZCj);
+simpTable. Properties. VariableNames (1:size (ZCj, 2) ) = {'x1','x2','s2','s3','a1','a2','sol'};
+disp(simpTable)
+
+RUN=true;
+while RUN
+    ZC = zjcj(:,1:end-1);
+    if any(ZC<0)
+        %finding the entering variable
+        [minVal,pivotCol] = min(ZC);
+        fprintf('the most negative element in zj-cj is %d corresponding to col %d \n',minVal,pivotCol);
+        fprintf('the entering varible is %d \n',pivotCol);
+
+        %finding the exiting variable
+        sol=A(:,end);
+        column = A(:,pivotCol);
+        if all(A(:,pivotCol)<=0)
+            error('LPP is unbounded. all enteries < 0 in column %d',pivotCol);
+        else
+            for i=1:size(column,1)
+                if column(i)>0
+                    ratio(i)=sol(i)./column(i);
+                else
+                    ratio(i)=Inf;
+                end
+            end
+            [minval,pivotRow]=min(ratio);
+            fprintf('the minimum ratio is %d corresponding to row %d \n',minval,pivotRow);
+            fprintf('the exiting varible is %d \n',pivotRow);
+        end
+        BV(pivotRow)=pivotCol;
+        disp('New Basic Variable (BV)')
+        disp(BV)
+        %pivot key 
+        pivotKey = A(pivotRow,pivotCol);
+        %update table for next phase
+        A(pivotRow,:)=A(pivotRow,:)/pivotKey;
+        for i=1:size(A,1)
+            if i~=pivotRow
+                A(i,:)=A(i,:)-A(i,pivotCol).*A(pivotRow,:);
+            end
+            zjcj = zjcj-zjcj(pivotCol).*A(pivotRow,:);
+        end
+        %for printing
+        ZCj = [zjcj;A];
+        Table = array2table(ZCj);
+        Table. Properties. VariableNames (1:size (ZCj, 2) ) = {'x1','x2','x3','s1','s2','s3','sol'};
+        disp(Table);
+
+        BFS=zeros(1,size(A,2));
+        BFS(BV)=A(:,end);
+        BFS(end)=sum(BFS.*C);
+        Current_BFS=array2table(BFS);
+        Current_BFS.Properties. VariableNames (1:size (ZCj, 2) ) = {'x1','x2','x3','s1','s2','s3','sol'};
+        disp(Current_BFS)
+    else
+        RUN = false;
+        disp('optimal solution reached')
+    end
+end
